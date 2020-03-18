897487215acfbcf6e6ddc46dbc5f3a17eb38b376
Denis Gidzun <denis.gidzun@gmail.com>
2020-03-19 01:07:46 +0200

    Labs

--- Lab1C++/Lab1C++.cpp
+++ Lab1C++/Lab1C++.cpp
@@ -1,20 +1,39 @@
-// Lab1C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+bool Compare(int, int);
+void Decrement(int&);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num1 = 34, num2 = 34;
+    Compare(num1,num2);
+    Decrement(num1);
+    cout << num1;
+}
+bool Compare(int num1, int num2)
+{
+    bool flag = false;
+    if ((num1 ^ num2) == 0)
+        flag = true;
+    return flag;
+}
+void Decrement(int &num1)
+{
+	int num2 = 1,temp=0;
+	temp = num1 & num2;
+	if (temp == 0)
+	{
+
+		while (temp == 0) {
+			temp = num1 & num2;
+			num1 = num1 ^ num2;
+			num2=num2 << 1;
+		}
+	}
+	else {
+		num1 = num1^num2;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Lab1CSharp/Program.cs
+++ Lab1CSharp/Program.cs
@@ -6,7 +6,37 @@ namespace Lab1CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("!");
+            int num1 = 34, num2 = 34;
+            bool result =Compare(num1,num2);
+            Decrement(ref num1);
+            Console.WriteLine(num1);
+
+        }
+        public static bool Compare(int num1,int num2)
+        {
+            bool flag = false;
+            if ((num1 ^ num2) == 0)
+                flag = true;
+            return flag;
+        }
+        public static void Decrement(ref int num1)
+        {
+            int num2 = 1, temp = 0;
+            temp = num1 & num2;
+            if (temp == 0)
+            {
+
+                while (temp == 0)
+                {
+                    temp = num1 & num2;
+                    num1 = num1 ^ num2;
+                    num2 = num2 << 1;
+                }
+            }
+            else
+            {
+                num1 = num1 ^ num2;
+            }
         }
     }
 }
