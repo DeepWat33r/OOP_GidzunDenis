f2400d6765a21475e0a0415e2f3f5b418d9668cb
Denis Gidzun <denis.gidzun@gmail.com>
2020-03-26 09:13:39 +0200

    Lab3

---
+++ Lab3CSharp/MyString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3CSharp
+{
+    public class MyString
+    {
+        private char[] s;
+        public MyString(string s2)
+        {
+            s = s2.ToCharArray();
+        }
+        public char this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < s.Length) return char.ToUpper(s[index]);
+                return '-';
+            }
+        }
+        public int GetLetter
+        {
+            get
+            {
+                char[] s2 = { 'a', 'e', 'i', 'u', 'o', 'y' };
+
+                int count = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    for (int j = 0; j < s2.Length; j++)
+                        if (s[i] == s2[j]) { count++; break; }
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Lab3CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyString s = new MyString("dsfo ewafwhee");
+            Console.WriteLine(s[2]);
+            Console.WriteLine(s.GetLetter);
+        }
+    }
+}
